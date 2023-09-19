f79460eddba65f1da240b35267acd0a8fe4658e1
lcerrotti <lautarocerrotti@gmail.com>
2023-09-19 03:28:48 -0300

    Creamos formularios , html , cargar y revisar datos desde db

--- meli_info/urls.py
+++ meli_info/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('hello/', views.hello),
-    path('about/', views.about)
+    path('', views.index, name="index"),
+    path('hello/', views.hello, name="hello"),
+    path('about/', views.about, name="about"),
+    path('username/<str:username>', views.username, name="username"),
+    path('project/', views.project, name="projects"),
+    path('task/', views.task,name="task"),
+    path('create_task/', views.create_task,name="Create_task"),
+    path('create_project/', views.create_project,name="Create_project"),
 ]
--- meli_info/views.py
+++ meli_info/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import Project, Task
+from django.shortcuts import get_list_or_404, render, redirect
+from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
+def index(request):
+    return render(request, 'index.html')
+
 def hello(request):
-    return HttpResponse("HELLO WORLD")
+    return render(request,"about.html")
 
 def about(request):
     return HttpResponse("ABOUT")
+
+def username(request, username):
+    return HttpResponse(f"{username}")
+
+def project(request):
+    projects = Project.objects.all()
+    return render(request,"projects.html", {
+        'projects': projects
+    })
+
+def task(request):
+    tasks = Task.objects.all()
+    return render(request,'task.html',{
+        'tasks':tasks
+    })
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html',{
+            'form':CreateNewTask()
+        })
+    else:
+        Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=1)
+        return redirect('/meli_info/task/')
+       
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'create_project.html',{
+            'form':CreateNewProject()
+        })
+    else:
+        project = Project.objects.create(name=request.POST['name'])
+        return redirect('projects')
+       
